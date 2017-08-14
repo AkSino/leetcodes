64ec99995f3b40a5e254f79fd7a544bb525bd0d7
Vardan <vardaan.gupta27@gmail.com>
2017-08-14 09:40:36 +0530

    Fibonacci_Checkin

---
+++ Fibonacci_recursion.py
@@ -0,0 +1,9 @@
+def Fibonacci(number_of_digits):
+    if number_of_digits==1:
+        return 0
+    if number_of_digits==2:
+        return 1
+    elif number_of_digits>2:
+        return Fibonacci(number_of_digits-1)+Fibonacci(number_of_digits-2)
+for i in range(10):
+    print(Fibonacci(i),end='')
